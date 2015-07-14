5705405c223912bf9b571790a2d446ddbccc8ae7
sankalp singh <sankalp_singh@symantec.com>
2015-07-14 12:24:00 +0530

    groovy perforce

--- test.groovy
+++ test.groovy
@@ -1,7 +1,15 @@
 job('DSL-Tutorial-1-Test') {
+
     scm {
-        perforce('ssl:perforce.ges.symantec.com:1666')
+      p4('testing') { node ->
+            node / configVersion(configVersion2)
+            node / p4Client("\${JOB_NAME}")
+            node / p4Port("server:1666")
+            node / disableChangeLogOnly(true)
+        }
     }
+
+
     triggers {
         scm('*/15 * * * *')
     }
